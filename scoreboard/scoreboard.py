3f1d17905dc29afcb81722954d6aaf48a7d540e4
MarcinC <chinek01@gmail.com>
2023-07-05 15:54:57 +0200

    scoreboard class init

---
+++ scoreboard/scoreboard.py
@@ -0,0 +1,21 @@
+"""
+
+Scoreboard
+
+Portfolio: Typing Speed Test
+#100DaysOfCode with Python
+Day: 85
+Date: 2023-07-05
+Author: MC
+
+Another version of this project. Previous wos so bad :)
+
+"""
+
+from tkinter.messagebox import showinfo
+
+
+class scoreboard:
+
+    def __init__(self):
+        pass
